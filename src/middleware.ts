54e6d120816cc4cefde5f182cbb3ea499e3ee515
choytr <143041680+choytr@users.noreply.github.com>
2025-02-14 03:43:59 -1000

    Maybe keep middleware.ts?

---
+++ src/middleware.ts
@@ -0,0 +1,35 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+export function middleware(request: NextRequest) {
+  const { pathname, hostname } = request.nextUrl;
+  if (!(
+    pathname.endsWith(".png") ||
+    pathname.endsWith(".jpg") ||
+    pathname.endsWith(".gif")
+  )) {
+    switch (hostname) {
+      case "rainfall.geography.hawaii.edu":
+        return NextResponse.rewrite(new URL("/rainfall" + pathname, request.url));
+      case "localhost":
+        return NextResponse.rewrite(new URL("/rainfall" + pathname, request.url));
+      case "climate.geography.hawaii.edu":
+        return NextResponse.rewrite(new URL("/climate" + pathname, request.url));
+      case "solarradiation.geography.hawaii.edu":
+        return NextResponse.rewrite(new URL("/solarradiation" + pathname, request.url));
+      case "evapotranspiration.geography.hawaii.edu":
+        return NextResponse.rewrite(new URL("/evapotranspiration" + pathname, request.url));
+      default:
+        return NextResponse.rewrite(new URL("/rainfall" + pathname, request.url));
+    }
+  }
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: [
+    {
+      source: '/((?!_next/static|_next/image|favicon.ico).*)',
+    },
+  ],
+};
