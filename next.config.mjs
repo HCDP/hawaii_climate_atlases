a8c52462fa7cdedd1340b1c9995832abeb4e4313
choytr <143041680+choytr@users.noreply.github.com>
2025-02-14 03:55:49 -1000

    Keep middleware.ts and erase next config contents

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  basePath: "/hawaii_climate_atlases/rainfall",
-  output: "export",
-};
+const nextConfig = {};
 
 export default nextConfig;
