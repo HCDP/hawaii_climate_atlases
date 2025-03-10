bf48f35d5108cfd8174b96cb35fae636e9b496f8
choytr <143041680+choytr@users.noreply.github.com>
2025-03-09 16:58:32 -1000

    Try using the actual link for assetPrefix

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",
   basePath: "/hawaii_climate_atlases/rainfall",
-  assetPrefix: "/hawaii_climate_atlases",
+  assetPrefix: "https://hcdp.github.io/hawaii_climate_atlases",
 };
 
 export default nextConfig;
