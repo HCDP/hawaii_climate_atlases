d628105f6a3155017a91a1b241919c6253007c9c
choytr <143041680+choytr@users.noreply.github.com>
2025-06-26 17:44:05 -1000

    Move the parsing logic to its own file instead of keeping it in the route handlers

--- src/app/rainfall/api/isohyets/[units]/route.ts
+++ src/app/rainfall/api/isohyets/[units]/route.ts
@@ -1,51 +1,14 @@
 import { NextRequest, NextResponse } from 'next/server';
-import shp, { FeatureCollectionWithFilename } from "shpjs";
 import { FeatureCollection } from "geojson";
-import { getCachedFileBuffer } from "@/lib/data_cache";
-import { Units } from "@/lib";
-import path from "path";
 import { isUnits } from "@/utils";
 import { invalidUnitsResponse, unableToRetrieveResponse } from "@/lib/responses";
-
-const CACHE_PATH = path.join('rainfall', 'raw');
-
-const IN_ISOHYETS_FILE_NAME = 'StateIsohyetsSHP_inches.zip';
-const IN_ISOHYETS_FILE_URL = new URL('https://atlas.uhtapis.org/rainfall/assets/files/GISLayers/StateIsohyetsSHP_inches.zip');
-
-const MM_ISOHYETS_FILE_NAME = 'StateIsohyetsSHP_mm.zip';
-const MM_ISOHYETS_FILE_URL = new URL('https://atlas.uhtapis.org/rainfall/assets/files/GISLayers/StateIsohyetsSHP_mm.zip');
+import { getIsohyets } from "@/lib/rainfall_data";
 
 export async function GET(_: NextRequest, { params }: { params: { units: string } }): Promise<NextResponse<{ error: string } | FeatureCollection[]>> {
   const units: string = params.units;
   if (!isUnits(units)) return invalidUnitsResponse;
-  let fileName, fetchUrl;
-  if (units === Units.IN) {
-    fileName = IN_ISOHYETS_FILE_NAME;
-    fetchUrl = IN_ISOHYETS_FILE_URL;
-  } else if (units === Units.MM) {
-    fileName = MM_ISOHYETS_FILE_NAME;
-    fetchUrl = MM_ISOHYETS_FILE_URL;
-  } else {
-    return invalidUnitsResponse;
-  }
 
-  const shpFileBuffer: Buffer | null = await getCachedFileBuffer(fetchUrl, CACHE_PATH, fileName);
-  if (!shpFileBuffer) {
-    return unableToRetrieveResponse;
-  }
-
-  let geojson: FeatureCollectionWithFilename | FeatureCollectionWithFilename[] = await shp(shpFileBuffer);
-  if (!Array.isArray(geojson)) {
-    geojson = [geojson];
-  } else {
-    const sortGeojsons = (a: FeatureCollectionWithFilename, b: FeatureCollectionWithFilename) => {
-      if (!a.fileName || !b.fileName) {
-        return 0;
-      }
-      return a.fileName > b.fileName ? 1 : a.fileName < b.fileName ? -1 : 0;
-    }
-    geojson.sort(sortGeojsons);
-    geojson.forEach(g => delete g.fileName);
-  }
+  const geojson = await getIsohyets({ units });
+  if (!geojson) return unableToRetrieveResponse;
   return NextResponse.json(geojson, { status: 200 });
 }
--- src/utils.ts
+++ src/utils.ts
@@ -11,6 +11,8 @@ export function isUnits(unitsString: string): unitsString is Units {
   return unitsString.toLocaleUpperCase() in Units;
 }
 
-export function isPeriod(periodString: string): periodString is Period {
-  return capitalize(periodString.toLocaleLowerCase()) in Period;
+export function isPeriod(periodString: string): periodString is keyof typeof Period {
+  return Object.keys(Period)
+    .filter(key => isNaN(Number(key)))
+    .includes(capitalize(periodString.toLowerCase()));
 }
